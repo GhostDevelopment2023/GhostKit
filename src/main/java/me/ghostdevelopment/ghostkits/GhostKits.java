3b25fa328f66165af6dcf14429a74d4c49a918dc
GhostAndry <andrearonzoni20062022@gmail.com>
2024-03-19 13:12:17 +0100

    Save 19/03/2024 13:12

--- src/main/java/me/ghostdevelopment/ghostkits/GhostKits.java
+++ src/main/java/me/ghostdevelopment/ghostkits/GhostKits.java
@@ -1,11 +1,22 @@
 package me.ghostdevelopment.ghostkits;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.SneakyThrows;
+import me.ghostdevelopment.ghostkits.Commands.GhostCommand;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.reflections.Reflections;
+
+import java.lang.reflect.InvocationTargetException;
 
 public final class GhostKits extends JavaPlugin {
 
+    @Getter @Setter
+    private static GhostKits instance;
+
     @Override
+    @SneakyThrows
     public void onEnable() {
 
         Bukkit.getLogger().info("\n\n" +
@@ -16,11 +27,30 @@ public final class GhostKits extends JavaPlugin {
                 "| |__| | | | | (_) \\__ \\ |_| . \\| | |_\\__ \\\n" +
                 " \\_____|_| |_|\\___/|___/\\__|_|\\_\\_|\\__|___/\n\n" +
                 "GhostKits by GhostAndry\n");
+        
+        setInstance(this);
+        registerCommands();
 
     }
 
-    @Override
-    public void onDisable() {
-        // Plugin shutdown logic
+    private void registerCommands() throws NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
+        String packageName = getClass().getPackage().getName();
+        for(Class<? extends GhostCommand> clazz: new Reflections(packageName + ".Commands.impl").getSubTypesOf(GhostCommand.class)){
+            GhostCommand command = null;
+
+            command = clazz.getDeclaredConstructor().newInstance();
+
+            try {
+                getCommand(command.getName()).setExecutor(command);
+                if(command.getTabComplete()){
+                    getCommand(command.getName()).setTabCompleter(command);
+                }
+            } catch (Exception e) {
+                Bukkit.getLogger().warning("[GhostKits] Can't load %module% module"
+                        .replace("%module%", command.getName())
+                );
+            }
+        }
     }
+
 }
